e3b28ab6fc8147b0cae348f1f504d731a1ccbac6
Tyson Jones <ballisticmissle@gmail.com>
2024-06-02 09:02:27 -0600

    got tests working

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,7 +3,7 @@
 // Demonstrate some basic assertions.
 TEST(machine_strike_engine_test, BasicAssertions) {
   // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
+  EXPECT_STRNE("hello", "hello");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
